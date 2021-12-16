ca3119b8126fe27659a55f8221e229162391b554
puszkarek <guipuszkarek@gmail.com>
2021-12-16 14:17:50 -0300

    move decoder to one file

--- src/models/indexer/index.ts
+++ src/models/indexer/index.ts
@@ -6,11 +6,9 @@
  * MIT license. See the LICENSE file for details.
  **************************************************************************/
 
-export * from './indexer-well-decoder';
 export * from './indexer-well';
 export * from './replicated-state';
 export * from './response-guard';
-export * from './shard-decoder';
 export * from './shard';
 export * from './to-indexer-well';
 export * from './well';
--- src/models/indexer/response-guard.ts
+++ src/models/indexer/response-guard.ts
@@ -6,7 +6,49 @@
  * MIT license. See the LICENSE file for details.
  **************************************************************************/
 
-import { guard } from 'decoders';
-import { indexerWellDecoder } from './indexer-well-decoder';
+import { array, boolean, dict, guard, iso8601, number, object, optional, string } from 'decoders';
+
+const shardDecoder = object({
+	Name: string,
+	Start: iso8601,
+	End: iso8601,
+	Entries: number,
+	Size: number,
+	Cold: boolean,
+	RemoteState: optional(
+		object({
+			UUID: string,
+			Entries: number,
+			Size: number,
+		}),
+	),
+});
+
+const indexerWellDecoder = dict(
+	object({
+		UUID: string,
+		Wells: array(
+			object({
+				Name: string,
+				Accelerator: optional(string),
+				Engine: optional(string),
+				Path: string,
+				Tags: array(string),
+				Shards: array(shardDecoder),
+			}),
+		),
+		Replicated: dict(
+			array(
+				object({
+					Name: string,
+					Accelerator: optional(string),
+					Engine: optional(string),
+					Tags: array(string),
+					Shards: array(shardDecoder),
+				}),
+			),
+		),
+	}),
+);
 
 export const responseGuard = guard(indexerWellDecoder);
